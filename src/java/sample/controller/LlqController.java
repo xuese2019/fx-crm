2888bc9ac06e84f3368c8b31576dc26b2d5b3a7e
shengzhizhe <15100162730@163.com>
2019-03-11 14:21:59 +0800

    lianxi

--- src/java/sample/controller/LlqController.java
+++ src/java/sample/controller/LlqController.java
@@ -1,6 +1,7 @@
 package sample.controller;
 
 import com.sun.javafx.robot.impl.FXRobotHelper;
+import javafx.application.Platform;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.ObservableList;
@@ -30,19 +31,25 @@ public class LlqController {
     private TextField paths;
 
     public void init() {
+        paths.setOnMouseClicked(event -> {
+            Platform.runLater(new Runnable() {
+                @Override
+                public void run() {
+                    paths.requestFocus();
+                }
+            });
+        });
 //        地址栏添加回车事件
         paths.setOnKeyReleased(event -> {
             if (KeyCode.ENTER.equals(event.getCode())) {
                 tz();
             }
         });
-        paths.setText("http://www.baidu.com");
         ObservableList<Stage> stages = FXRobotHelper.getStages();
         Stage stage = stages.get(0);
         Scene scene = stage.getScene();
         anch.setMinHeight(scene.getHeight() - 30);
         WebEngine engine = webs.getEngine();
-        engine.load(paths.getText());
 //        监听地址改变
         engine.getLoadWorker().stateProperty().addListener(
                 new ChangeListener<Worker.State>() {
@@ -52,13 +59,18 @@ public class LlqController {
                         }
                     }
                 });
+        tz();
     }
 
     @FXML
     private void tz() {
         String text = paths.getText();
         if (text != null && !text.trim().isEmpty()) {
-            webs.getEngine().load(text.trim());
+            if (text.contains("http://") || text.contains("https://")) {
+                webs.getEngine().load(text.trim());
+            } else {
+                webs.getEngine().load("https://www.baidu.com/s?ie=UTF-8&wd=" + text.trim());
+            }
         }
     }
 
@@ -68,7 +80,7 @@ public class LlqController {
     }
 
     @FXML
-    private void fh(){
+    private void fh() {
         WebHistory history = webs.getEngine().getHistory();
         int currentIndex = history.getCurrentIndex();
         if (currentIndex > 0)
